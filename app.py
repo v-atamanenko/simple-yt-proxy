ddf65adf7aa6ec877025f61566db1eb6b7e95335
v-atamanenko <v@atamanenko.in>
2022-10-12 17:03:06 +0400

    add info endpoint

--- app.py
+++ app.py
@@ -21,6 +21,63 @@ CORS(app)
 #redis_conn = Redis(host="127.0.0.1", port=6379)
 #q = Queue(connection=redis_conn)
 
+from pytube import YouTube
+from pytube import Channel
+import time
+import json
+
+def parseSubscriberCount(subs):
+    subs = subs.replace(" subscribers", "")
+    subs_i = 0
+    multiplier = 1
+
+    try:
+        if subs[-1] == "B":
+            multiplier = 1000000000
+        if subs[-1] == "M":
+            multiplier = 1000000
+        if subs[-1] == "K":
+            multiplier = 1000
+        subs = subs[:-1]
+
+        subs_i = float(subs) * multiplier
+    except Exception as e:
+        raise(e)
+        subs_i = 1
+
+    return subs_i
+
+
+def getVideoInfo(url):
+    yt = YouTube('https://www.youtube.com/watch?v=koxXu5Wa11I')
+    ch = Channel(yt.channel_url)
+
+    res = {}
+    res["id"] = yt.vid_info["videoDetails"]["videoId"]
+    res["title"] = yt.title
+    res["duration_seconds"] = yt.length
+    res["published_at"] = time.mktime(yt.publish_date.timetuple())
+    res["thumbnail_url"] = yt.thumbnail_url
+    res["description_long"] = yt.description
+    res["view_count"] = yt.views
+    res["author_id"] = ch.channel_id
+    res["author_name"] = ch.channel_name
+    try:
+        res["author_thumbnail_url"] = ch.initial_data["header"]["c4TabbedHeaderRenderer"]["avatar"]["thumbnails"][-1]["url"]
+        res["author_subscriptions_count"] = parseSubscriberCount(ch.initial_data["header"]["c4TabbedHeaderRenderer"]["subscriberCountText"]["simpleText"])
+    except Exception as e:
+        raise(e)
+        res["author_thumbnail_url"] = res["id"]
+        res["author_subscriptions_count"] = 0
+
+    try:
+        res["like_count"] = int(yt.initial_data["engagementPanels"][1]["engagementPanelSectionListRenderer"]["content"]["structuredDescriptionContentRenderer"]["items"][0]["videoDescriptionHeaderRenderer"]["factoid"][0]["factoidRenderer"]["value"]["simpleText"])
+    except Exception as e:
+        res["like_count"] = 1
+
+    return res
+
+
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
@@ -57,6 +114,31 @@ def comments():
     response = jsonify(Comments.get(video_id))
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
+
+
+@app.route('/info', methods=['GET', 'POST'])
+def info():
+    if request.method != 'POST':
+        response = jsonify({
+            "error": "Unknown request method"
+        })
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
+
+    video_id = str(request.json.get('video_url', ''))
+    video_id = video_id.replace("clipzag.com", "youtube.com")
+
+    try:
+        response = jsonify(getVideoInfo(video_id))
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
+    except Exception as e:
+        response = jsonify({
+            "error": "Failed to get video info: " + str(e)
+        })
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
+
 
 #def schedule_task(things, stream):
 #    job = Job.create(
